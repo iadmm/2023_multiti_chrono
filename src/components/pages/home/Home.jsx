2bb579cc223a78fd02a0c8b17efec7aef42fa559
Julien Moreau <julien@thylo.be>
2023-01-22 11:52:36 +0100

    Add spacing classes

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Multiti Jam</h1>
+            <h1 className="u-margin-bottom-none">Multiti Jam</h1>
             <h2>2023</h2>
             <p>Creation de projets en 48h</p>
         </div>
