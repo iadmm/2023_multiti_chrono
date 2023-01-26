e11bd798608b9d5d565c864f3935325597aca349
martincarpentier2001 <48276606+martincarpentier2001@users.noreply.github.com>
2023-01-26 11:27:54 +0100

    User page + Martin's CSS

--- src/components/pages/user/User.jsx
+++ src/components/pages/user/User.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const User = () => {
     return (
-        <div>
-            <button>Log in</button>
-            <form>
-                <label>
-                    Collez l'URL de l'image, du GIF ou de la vidéo Youtube ici : 
+        <div className='divcontainer l-container l-container--copy'>
+            <form className='formflex'>
+                <p className='text-label'>Titre de votre contenu :</p>
+                <label> 
+                    <input type="text" name="URL" />
+                </label>
+                <p className='text-label'>Collez l'URL de l'image, du GIF ou de la vidéo Youtube ici :</p>
+                <label> 
                     <input type="text" name="URL" />
                 </label>
                 <button type="submit">Envoyer</button>
