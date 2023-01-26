bd1fd974504622f530d2d5020f67fdb698193f86
Julien Moreau <julien@thylo.be>
2023-01-26 18:02:08 +0100

    Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import {
   Link,
-  Navigate,
   useNavigate,
-  useRouteLoaderData,
 } from "react-router-dom";
-import { Session } from "@supabase/gotrue-js";
 
 const Home = () => {
   const navigate = useNavigate();
-  const { session } = useRouteLoaderData("root") as { session: Session | null };
-  if (session) {
-    return <Navigate to={"/account"} />;
-  }
+
   return (
     <div>
       <h1 className="u-margin-bottom-none">Multiti Jam</h1>
