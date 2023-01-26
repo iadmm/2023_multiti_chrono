87302bab355d164a5f2140cf8561b2b1514e22de
Julien Moreau <julien@thylo.be>
2023-01-26 17:44:58 +0100

    Prettier

--- src/pages/player/components/VideoPlayer.jsx
+++ src/pages/player/components/VideoPlayer.jsx
@@ -1,8 +1,14 @@
 import ReactPlayer from "react-player";
 import React from "react";
 
-<<<<<<< HEAD
-const VideoPlayer = ({ slide, goToNextSlide }) => {
+const VideoPlayer = ({ slide, goToNextSlide, maxLength, loop = false }) => {
+  function onProgress({ playedSeconds }) {
+    console.log(playedSeconds);
+    if (playedSeconds > maxLength) {
+      goToNextSlide();
+    }
+  }
+
   return (
     <ReactPlayer
       url={slide.value}
@@ -10,23 +16,10 @@ const VideoPlayer = ({ slide, goToNextSlide }) => {
       height="100%"
       playing
       onEnded={goToNextSlide}
+      onProgress={onProgress}
+      loop={loop}
     />
   );
 };
-=======
-const VideoPlayer = ({slide, goToNextSlide, maxLength, loop = false}) => {
-
-    function onProgress({playedSeconds}) {
-        console.log(playedSeconds)
-        if (playedSeconds > maxLength) {
-            goToNextSlide();
-        }
-
-    }
-
-    return <ReactPlayer url={slide.value} width="100%" height="100%" playing onEnded={goToNextSlide}
-                        onProgress={onProgress} loop={loop}/>
-}
->>>>>>> 90eefb247d8fee167a591404942e6a0fc95ab87e
 
 export default VideoPlayer;
