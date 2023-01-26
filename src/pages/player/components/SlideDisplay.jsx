221f6b07eb9e1c54a1d982690232fb98386052fe
Théodore van der Rest <66850239+HerodoteVDR@users.noreply.github.com>
2023-01-26 17:26:05 +0100

    data random

--- src/pages/player/components/SlideDisplay.jsx
+++ src/pages/player/components/SlideDisplay.jsx
@@ -2,17 +2,57 @@ import React from "react";
 import VideoPlayer from "./VideoPlayer.jsx";
 import ImagePlayer from "./ImagePlayer.jsx";
 
-const SlideDisplay = ({slide, goToNextSlide}) =>{
-    if (!slide){
-        return <div>Prout il n'y a rien</div>
-    }
-    if (slide.type == "video"){
+const defaultSlides = [{
+    value: "https://www.youtube.com/watch?v=HpiE45VVlf8",
+    title: "fireplace"
+},
+    {
+    value: "https://www.youtube.com/watch?v=G1IbRujko-A&t=110s",
+    title: "fireplace"
+},
+    {
+    value: "https://www.youtube.com/watch?v=ggDbDRUauzo&t=1s",
+    title: "duck"
+},
+    {
+    value: "https://www.youtube.com/watch?v=t6isux5XWH0",
+    title: "rat"
+},
+    {
+    value: "https://www.youtube.com/watch?v=hCC6ZhRS7sY",
+    title: "cowboy"
+},
+    {
+    value: "https://www.youtube.com/watch?v=hCC6ZhRS7sY",
+    title: "cowboy"
+},
+    {
+    value: "https://www.youtube.com/watch?v=L_6Q28__BCI&t=2s",
+    title: "boom"
+},
+    {
+    value: "https://www.youtube.com/watch?v=CEXAvpACHf0",
+    title: "hello there"
+},
+
+];
+
+const SlideDisplay = ({slide, goToNextSlide}) => {
+    if (!slide) {
+        const fakeSlideIndex = Math.floor(Math.random() * defaultSlides.length);
+        console.log({fakeSlideIndex})
+        const fakeSlide = defaultSlides[fakeSlideIndex];
         return <div className="c-slide_display">
-            <VideoPlayer slide={slide} goToNextSlide={goToNextSlide}/>
+            <VideoPlayer slide={fakeSlide} goToNextSlide={goToNextSlide} loop={true} maxLength={60 * 60 * 24}/>
+        </div>
+    }
+    if (slide.type === "video") {
+        return <div className="c-slide_display">
+            <VideoPlayer slide={slide} goToNextSlide={goToNextSlide} maxLength={60 * 4}/>
         </div>;
     }
     return <div className="c-slide_display">
-        <ImagePlayer slide={slide} goToNextSlide={goToNextSlide} />
+        <ImagePlayer slide={slide} goToNextSlide={goToNextSlide}/>
     </div>;
 
 }
--- src/pages/player/components/VideoPlayer.jsx
+++ src/pages/player/components/VideoPlayer.jsx
@@ -1,8 +1,18 @@
 import ReactPlayer from "react-player";
 import React from "react";
 
-const VideoPlayer = ({slide,goToNextSlide}) => {
-    return <ReactPlayer url={slide.value} width="100%" height="100%" playing onEnded={goToNextSlide} />
+const VideoPlayer = ({slide, goToNextSlide, maxLength, loop = false}) => {
+
+    function onProgress({playedSeconds}) {
+        console.log(playedSeconds)
+        if (playedSeconds > maxLength) {
+            goToNextSlide();
+        }
+
+    }
+
+    return <ReactPlayer url={slide.value} width="100%" height="100%" playing onEnded={goToNextSlide}
+                        onProgress={onProgress} loop={loop}/>
 }
 
 export default VideoPlayer;
