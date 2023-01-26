b83dc58ae79871865396fece43b2b8ee4e59bb8a
Julien Moreau <julien@thylo.be>
2023-01-26 17:43:41 +0100

    Alter package json

--- src/pages/player/components/VideoPlayer.jsx
+++ src/pages/player/components/VideoPlayer.jsx
@@ -1,8 +1,16 @@
 import ReactPlayer from "react-player";
 import React from "react";
 
-const VideoPlayer = ({slide,goToNextSlide}) => {
-    return <ReactPlayer url={slide.value} width="100%" height="100%" playing onEnded={goToNextSlide} />
-}
+const VideoPlayer = ({ slide, goToNextSlide }) => {
+  return (
+    <ReactPlayer
+      url={slide.value}
+      width="100%"
+      height="100%"
+      playing
+      onEnded={goToNextSlide}
+    />
+  );
+};
 
 export default VideoPlayer;
